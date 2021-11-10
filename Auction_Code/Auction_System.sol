49f7c86e820caeba32c8ffc735444f7bc544eb01
Tyler Donovan <89551524+DonovanTyler@users.noreply.github.com>
2021-11-10 14:32:09 -0800

    swag

--- Auction_Code/Auction_System.sol
+++ Auction_Code/Auction_System.sol
@@ -1,22 +1,35 @@
 pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 contract Auction_System {
     
 }
-contract WinAuction{
+contract WinAuction {
+    
     event Win(address winner, uint256 amount);
-    IERC721 public immutable nft;
-    uint public immutable nftID;
-    constructor (address _nft, _nftId) {
+
+    IERC721 public nft;
+    uint public nftId;
+
+    address payable public seller;
+    address public winner;
+
+    constructor(
+        address _nft,
+        uint _nftId
+    ) {
         seller = payable(msg.sender);
         nft = IERC721(_nft);
         nftId = _nftId;
     }
+
+    // Winds the auction for the specified amount
     function win() external payable {
         winner = msg.sender;
-        nft.transferFrom(seller, msg.sender, nftId);
+        nft.safeTransferFrom(seller, msg.sender, nftId);
         seller.transfer(msg.value);
-        
+
         emit Win(msg.sender, msg.value);
     }
 }
