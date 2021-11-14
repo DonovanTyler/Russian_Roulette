7ae91d6a9717d7bfc2f69f6b92a11584f3614ae0
Tyler Donovan <89551524+DonovanTyler@users.noreply.github.com>
2021-11-14 08:45:16 -0800

    Made some changes ig

--- Auction_Code/Auction_System.sol
+++ Auction_Code/Auction_System.sol
@@ -2,9 +2,52 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract Auction_System {
-    
+contract AuctionSystem {
+    string itemName;
+	uint256 currentPrice;
+	uint256 finalPrice;
+	string description;
+	uint256 minimumPrice;
+	uint256 bidTime;
+	uint256 minimumIncrement;
+	string currency;
+	IERC721 nsfw;
+    function openAuction (uint256 minPrice, uint256 time, IERC721 nft, string memory descrip, uint256 increment) public {
+        minimumPrice = minPrice;
+	    bidTime = time;
+        nsfw = nft;
+        description = descrip;
+	    minimumIncrement = increment;
+    }
 }
+
+contract Bidder {
+address highestBidder;
+string itemName;
+uint256 popularity;
+uint256 currentPrice;
+uint256 timeLeft;
+uint256 bidCount;
+IERC721 item;
+string description;
+uint256 timeExtensions = 0;
+
+function bid(uint256 amount) public {
+    if(amount > currentPrice)
+	{
+		currentPrice = amount;
+		if(timeLeft >= 10 && timeExtensions < 10)
+		{
+			timeLeft = 10;
+			timeExtensions++;
+		}
+}
+}
+
+
+}
+
+
 contract WinAuction {
     
     event Win(address winner, uint256 amount);
@@ -23,10 +66,12 @@ contract WinAuction {
         nft = IERC721(_nft);
         nftId = _nftId;
     }
-
+    modifier onlyWinner {
+        require(msg.sender == winner);
+        _;
+    }
     // Winds the auction for the specified amount
-    function win() external payable {
-        winner = msg.sender;
+    function win() external payable onlyWinner{
         nft.safeTransferFrom(seller, msg.sender, nftId);
         seller.transfer(msg.value);
 
